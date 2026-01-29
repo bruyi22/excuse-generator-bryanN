54ab5239900b0f0497bd14a3be05b92b68c86f1a
Bryan Navarro <diazbnavarro@gmail.com>
2026-01-29 18:37:29 +0000

    added generated excuse at onload

--- src/app.js
+++ src/app.js
@@ -8,4 +8,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  const excuseElement = document.querySelector('#excuse');
+    const button = document.querySelector('#generate-btn');
+
+    // 2. Define components
+    const who = ['The dog', 'My grandma', 'A ghost', 'A turtle', 'My boss'];
+    const action = ['ate', 'destroyed', 'lost', 'stole', 'hid'];
+    const what = ['my project', 'the keys', 'my motivation', 'my phone'];
+    const when = ['this morning', 'just now', 'last night', 'during lunch'];
+
+    // 3. Generating Function
+    const generateExcuse = () => {
+        let rdmWho = who[Math.floor(Math.random() * who.length)];
+        let rdmAction = action[Math.floor(Math.random() * action.length)];
+        let rdmWhat = what[Math.floor(Math.random() * what.length)];
+        let rdmWhen = when[Math.floor(Math.random() * when.length)];
+
+        return `${rdmWho} ${rdmAction} ${rdmWhat} ${rdmWhen}.`;
+    };
+
+    // 4. Show first excuse at onload
+    excuseElement.innerHTML = generateExcuse();
+
+    // 5. Generate excuse
+    
+
 };
